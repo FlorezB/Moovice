3acd9ee8cf5dbffa5e8df3c4e05cdad676067b73
Caroline <alyseflorez@hotmail.fr>
2022-08-03 18:24:28 +0200

    setup majuscule

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <>
             <h1>Home</h1>
-            // useEffect(() => {
+            {/* // useEffect(() => {
             //   fetchData()
             // }, [])
 
@@ -11,7 +11,7 @@ const Home = () => {
             // const filmsData = async () => {
             //   const request = await fetch ('https://api.themoviedb.org/3/movie/550?api_key=0c9ba7c2d018a721dfbefc77c52e32cd')
             //   const response = await request.json()
-            // }
+            // } */}
         </>
     )
 }
